f9f2d241650853e69d8590c1be72d362064c7648
clifford dicarlo <cdc4444@gmail.com>
2020-08-14 11:06:57 -0400

    2020-08-14T10:58:55 Updated this version of fabricateSQL by;   a) eliminated unused  variables (`//=` identifies these unused variable) found in header file named fabricateSQL.h;   b) dynamically allocated sufficient (12½ % more than needed) storage for containing the input template;   c) added to destructor the ability to delete the dynamically allocated storage. 2020-08-13T11:37:26 Works as a dylib when called by a test driver like: /Users/cjc/Library/Developer/Xcode/DerivedData/Build/Products/Debug/testingFabricateSQL3 28 2020-08-09 90.583333 4 4 SR x 0) the name of the test driver program is /Users/cjc/Library/Developer/Xcode/DerivedData/Build/Products/Debug/testingFabricateSQL3; 1) the next (first) positional parameter, 28, represents the site id as described in LocalWeather database's table named: tbl_site_names; 2) the second positional parameter, 2020-08-09, represents that date of interest for us to gather data surrounding what we believe to be the time of sunset or sunrise (as determined by the sixth position parameter); 3) the third positional parameter, 90.583333, represents the zenith distance (elevation angle = 90˚ - zenith distance, or, zenith distance = 90˚ - elevation angle) which defines sunrise or sunset; 4) the fourth positional parameter represents the number of tuples prior to sunrise or sun set (as per sixth positional parameter); 5) the fifth positional parameter represents the number of tuples after sunrise or sunset (as per sixth positional parameter); 6) the sixth positional parameter determines if we will be seeking data around sunrise or sunset: if this sixth positional parameter is `SR`, `SunRise`, or `SUNRISE` then we will seek data around sun rise; if this sixth positional parameter is `SS`, `SunSet`, or `SUNSET` then we will seek data around sun set. 7) the seventh positional parameter indicates if we want to see intermediate data for debugging. The presence of any single alpha or numeric character will result in the output of intermediate processing data useful for debugging; the absence of the seventh positional parameter precludes the output of intermediate processing data.

--- fabricateSQLh.hpp
+++ fabricateSQLh.hpp
@@ -29,7 +29,7 @@ using namespace std;
 //"SRorSS" = '>' means doing SunSet; "SRorSS" = "<" means doing SunRise.
 class SS {
 private:
-    const std::string default_template = \
+    const string default_template = \
     "SELECT * FROM tbl_sun_loc_site \
     WHERE lt::DATE='_DATE_' AND siteid=SITEID AND id BETWEEN \
     -BEFORESUNSET + ( SELECT id FROM (SELECT *,abs(zenithdistance-SUNSETDEF) \
@@ -44,64 +44,68 @@ private:
     (SELECT min(abs(zenithdistance-SUNSETDEF)) FROM tbl_sun_loc_site WHERE lt::DATE='_DATE_' AND siteid=SITEID AND events IS NULL AND id SRorSS \
     (SELECT avg(id) FROM tbl_sun_loc_site WHERE lt::DATE='_DATE_' AND siteid=SITEID AND events IS NULL))   ) \
     ORDER BY id;";
-    const int len_sunsetSQL_template = (int)default_template.length(); //Static is NOT needed because length of default_template is determined during \
-    the execution phase.
-    static const int templateBufferSize = 2500; //Needs static because the value, to the right of the `=` sign, \
+    const int len_sunsetSQL_template = (int)default_template.length(); //Static is NOT needed because length of default_template \
+    is determined during the execution phase.
+//=    static const int templateBufferSize = 2500; //Needs static because the value, to the right of the `=` sign, \
     is assigned during compilation
-    static const int arrayBufferSize = 32;
-    static const int arrayOfCopy = 20; //Enough space to hold 20 pointers to tokens and 20 pointers to replacements.
+//=    static const int arrayBufferSize = 32;
+//=    static const int arrayOfCopy = 20; //Enough space to hold 20 pointers to tokens and 20 pointers to replacements.
+    static char *const ptr_workArrayTemplate; //= workArrayTemplate;
     static const int zero=0;
+    static const int numberOfPointerArrayElements = 8;
     const multimap<string, string>  dictionary  = {
-        { "SR", "<"},
+        {"SR", "<"},
         {"SunRise", "<" },
         {"SUNRISE", "<"},
         {"SS", ">"},
         {"SunSet", ">"},
         {"SUNSET", ">"}
     };
-
+//
 public:
 
-    char workArrayRepl[arrayBufferSize];
-    char *ptr_workArrayRepl;
-    char workArrayToken[arrayBufferSize];
-    char *ptrOfCopyOfTokenArray[arrayOfCopy];
-    char *ptrOfCopyOfReplacementArray[arrayOfCopy];
-    char *ptr_workArrayToken;
+    //=char workArrayRepl[arrayBufferSize] ={0};
+    //=char *ptr_workArrayRepl;
+    //=char workArrayToken[arrayBufferSize] = {0};
+    //=char *ptrOfCopyOfTokenArray[arrayOfCopy];
+    //=char *ptrOfCopyOfReplacementArray[arrayOfCopy] = {0};
+    //=char *ptr_workArrayToken;
     int i;
     int j;
-    int kk;
+    int kk; 
     int number_of_tokens_passed_by_calling_program;
     int inputTemplateLength;
     long resultingStringLength, resultingStringSize;
     unsigned long  arraySize, templateSize;
     bool debugMode;
-    char lastChar;
-    static long *pos; /*Declaration of variable 'position' with deduced type 'auto' requires an initializer */
-    std::string sross;
+    //=char lastChar;
+    //=static long *pos; /*Declaration of variable 'position' with deduced type 'auto' requires an initializer */
+    std::string sross; 
     std::string inputTemplate;
-    std::string result1;
+    std::string result1; 
     string work;
     string work2;
-    string stdStringArrayOfReplacementStrings[16];
+    //=string stdStringArrayOfReplacementStrings[numberOfPointerArrayElements];
     unsigned int lengthOfCallersTemplate;
     const char *sr_or_ss = "SRorSS";
-    char cstring_workArray[16];
-    char *ptr_workArray;
-    char **ptrptrcopyOfReplacements[8];
-    char **ptrptrcopyOfTokens[8];
-    char *ptrcopyOfReplacements[8];
-    char *ptrcopyOfTokens[8];
+    char cstring_workArray[numberOfPointerArrayElements];
+    //=char *ptr_workArray;
+    char **ptrptrcopyOfReplacements[numberOfPointerArrayElements];
+    char **ptrptrcopyOfTokens[numberOfPointerArrayElements];
+    char *ptrcopyOfReplacements[numberOfPointerArrayElements]; 
+    char *ptrcopyOfTokens[numberOfPointerArrayElements];
     string rpl; //Once converted to standard string from c-string, Stick the token's replacement value here
     std::regex tokenAsRegularExpression;
-    int caller_supplied_buffer_size = 0;
+    int caller_supplied_buffer_size = zero;
     int cStringLength;
-    int rc;
-    char workArrayTemplate[templateBufferSize]; //Space reserved for holding, as a c-string, standard string found in  default_template, above.
+    int rc = zero;
+//=    char workArrayTemplate[templateBufferSize]; //Space reserved for holding, as a c-string, standard string found in \
+    default_template, above.
+
     SS(int, char *, int, char **, char **, bool);
     ~SS();
     void doFabricateSQL(int, char **, char **, char *, char *, int, bool);
-    
+    string ptfss(string, string );
 };
 
 
